3e527adc3c0e2c6aabaf4ca4776d8d0a773b681d
ZhangWenZna <84662944+ZhangWenZna@users.noreply.github.com>
2022-02-21 23:14:56 +0800

    Initial commit

---
+++ src/main/java/com/zwz/jxone/RealRiskApplication.java
@@ -0,0 +1,13 @@
+package com.zwz.jxone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealRiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zwz/jxone/RealRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwz.jxone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
