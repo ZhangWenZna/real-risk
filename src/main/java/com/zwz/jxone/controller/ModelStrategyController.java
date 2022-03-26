c671d9ecb090286d31fdd00aec66afe84508e755
周文卫 <zhouwenwei@Jwenwei-MacBook-Pro.local>
2022-03-26 21:53:26 +0800

    fix

--- src/main/java/com/zwz/jxone/controller/ModelStrategyController.java
+++ src/main/java/com/zwz/jxone/controller/ModelStrategyController.java
@@ -11,9 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 @RequestMapping(path = "/modelStrategies")
 public class ModelStrategyController {
     @Autowired
